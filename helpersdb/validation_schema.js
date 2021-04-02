9e1a128e8e0305c76e394f25da35b38efd6ed158
krishna <officialbandari6@gmail.com>
2021-04-02 12:17:37 -0700

    hashpasswordwithbcrypt

--- helpersdb/validation_schema.js
+++ helpersdb/validation_schema.js
@@ -1,5 +1,6 @@
 const Joi = require('@hapi/joi');
 
+
 const authSchema = Joi.object({
     email :Joi.string().email().lowercase().required(),
     password : Joi.string().min(2)
--- models/User.model.js
+++ models/User.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
 
 
 const UserSchema = mongoose.Schema({
@@ -15,6 +16,20 @@ const UserSchema = mongoose.Schema({
     }
 })
 
+UserSchema.pre('save', async function(next){
+
+    try {
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(this.password, salt)
+        this.password = hashedPassword
+        next()
+
+    } catch (error) {
+        next(error)
+        
+    }
+
+});
 
 const User = mongoose.model('user', UserSchema)
 
